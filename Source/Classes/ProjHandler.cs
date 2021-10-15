b77279093424b28a5d4d558ac1be4e46b54888e6
Storm <92469024+Storm092@users.noreply.github.com>
2021-10-15 20:09:21 +0200

    Add files via upload

---
+++ Source/Classes/ProjHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Viper.Properties;
+
+namespace Viper.Source.Classes
+{
+    class ProjHandler
+    {
+        public void Handle(TreeView treeView1)
+        {
+            foreach (var dirs in Directory.GetDirectories(Settings.Default.projectDirectory))
+            {
+                DirectoryInfo DirInfo = new DirectoryInfo(dirs);
+                var nodes = treeView1.Nodes.Add(DirInfo.Name, DirInfo.Name, 0);
+                nodes.Tag = DirInfo;
+            }
+            foreach (var Files in Directory.GetFiles(Settings.Default.projectDirectory))
+            {
+                FileInfo fileInfo = new FileInfo(Files);
+                var nodes = treeView1.Nodes.Add(fileInfo.Name, fileInfo.Name, 1);
+                nodes.Tag = fileInfo;
+            }
+        }
+    }
+}
---
+++ Source/GUI/UI.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Viper
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.siticoneCustomGradientPanel1 = new Siticone.UI.WinForms.SiticoneCustomGradientPanel();
+            this.siticoneElipse1 = new Siticone.UI.WinForms.SiticoneElipse(this.components);
+            this.siticoneCustomGradientPanel2 = new Siticone.UI.WinForms.SiticoneCustomGradientPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.compilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siticoneCustomGradientPanel1.SuspendLayout();
+            this.siticoneCustomGradientPanel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // siticoneCustomGradientPanel1
+            // 
+            this.siticoneCustomGradientPanel1.Controls.Add(this.textBox1);
+            this.siticoneCustomGradientPanel1.Controls.Add(this.siticoneCustomGradientPanel2);
+            this.siticoneCustomGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.siticoneCustomGradientPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.siticoneCustomGradientPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.siticoneCustomGradientPanel1.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.siticoneCustomGradientPanel1.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.siticoneCustomGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.siticoneCustomGradientPanel1.Name = "siticoneCustomGradientPanel1";
+            this.siticoneCustomGradientPanel1.ShadowDecoration.Parent = this.siticoneCustomGradientPanel1;
+            this.siticoneCustomGradientPanel1.Size = new System.Drawing.Size(1136, 676);
+            this.siticoneCustomGradientPanel1.TabIndex = 0;
+            this.siticoneCustomGradientPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.siticoneCustomGradientPanel1_Paint);
+            // 
+            // siticoneElipse1
+            // 
+            this.siticoneElipse1.BorderRadius = 20;
+            this.siticoneElipse1.TargetControl = this;
+            // 
+            // siticoneCustomGradientPanel2
+            // 
+            this.siticoneCustomGradientPanel2.Controls.Add(this.treeView1);
+            this.siticoneCustomGradientPanel2.Controls.Add(this.menuStrip1);
+            this.siticoneCustomGradientPanel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.siticoneCustomGradientPanel2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.siticoneCustomGradientPanel2.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.siticoneCustomGradientPanel2.FillColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.siticoneCustomGradientPanel2.FillColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.siticoneCustomGradientPanel2.Location = new System.Drawing.Point(0, 0);
+            this.siticoneCustomGradientPanel2.Name = "siticoneCustomGradientPanel2";
+            this.siticoneCustomGradientPanel2.ShadowDecoration.Parent = this.siticoneCustomGradientPanel2;
+            this.siticoneCustomGradientPanel2.Size = new System.Drawing.Size(186, 676);
+            this.siticoneCustomGradientPanel2.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.treeView1.ForeColor = System.Drawing.Color.White;
+            this.treeView1.Location = new System.Drawing.Point(0, 28);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(186, 648);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            this.treeView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(64)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(192, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(932, 670);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compilerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(186, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // compilerToolStripMenuItem
+            // 
+            this.compilerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compileToolStripMenuItem});
+            this.compilerToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.compilerToolStripMenuItem.Name = "compilerToolStripMenuItem";
+            this.compilerToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.compilerToolStripMenuItem.Text = "Compiler";
+            // 
+            // compileToolStripMenuItem
+            // 
+            this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
+            this.compileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.compileToolStripMenuItem.Text = "Compile";
+            this.compileToolStripMenuItem.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1136, 676);
+            this.Controls.Add(this.siticoneCustomGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "UI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UI";
+            this.siticoneCustomGradientPanel1.ResumeLayout(false);
+            this.siticoneCustomGradientPanel2.ResumeLayout(false);
+            this.siticoneCustomGradientPanel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneCustomGradientPanel siticoneCustomGradientPanel1;
+        private Siticone.UI.WinForms.SiticoneCustomGradientPanel siticoneCustomGradientPanel2;
+        private System.Windows.Forms.TreeView treeView1;
+        private Siticone.UI.WinForms.SiticoneElipse siticoneElipse1;
+        private System.Windows.Forms.RichTextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem compilerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem;
+    }
+}
+
